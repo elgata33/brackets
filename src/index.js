ef7e4c22540a6051d9978f97b445a6167a28cdbd
elgata33 <garashov123@gmail.com>
2021-03-15 20:56:43 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let joinedBrackets = bracketsConfig.map((n) => n.join(""));
+
+    for (let i = 0; i < joinedBrackets.length; i++) {
+        if (str.includes(joinedBrackets[i])) {
+            str = str.replace(joinedBrackets[i], "");
+            i = -1;
+        }
+    }
+
+    if (str === "") {
+        return true;
+    }
+
+    return false;
+};
